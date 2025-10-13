b555c5c87f5fbaafd504f36aca16e95968eaafcc
Manju Srinivasa <msrinivasa@microsoft.com>
2025-10-13 13:21:37 -0700

    Brought together my-org-customizations.el and my-emacs-customizations.el into init.el

--- init.el
+++ init.el
@@ -1,1 +1,21 @@
-;; Intialize EMACS
+;; EMACS startup configuration
+
+;; Package system initialize
+(package-initialize)
+
+
+;; Default directory
+(setq default-directory (expand-file-name "~/org/"))
+
+;; Load path for custom modules
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
+
+;; Load customizations
+(load "my-emacs-customizations")
+(load "my-org-customizations")
+
+;; Open inbox.org on startup
+(setq initial-buffer-choice (expand-file-name "~/org/inbox.org"))
+
+(provie 'init)
+;; init.el ends here
